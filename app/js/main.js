bf2b7a07f3b98a55bcb21522aa18c9f78656414a
igorek19882020 <igorek20081988@mail.ru>
2022-02-03 21:58:47 +0300

    product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,69 @@
 $(function () {
-    $('.banner__section-slider').slick({
-        dots: true,
-        prevArrow: '<button class="banner__section-slider-btn banner__section-slider-btnprev"><img src="images/left-arrow.svg"" alt=""></button>',
-        nextArrow: '<button class= "banner__section-slider-btn banner__section-slider-btnnext " > <img src="images/right-arrow.svg" alt=""></button>'
-    });
+  $(".banner__section-slider").slick({
+    dots: true,
+    prevArrow:
+      '<button class="banner__section-slider-btn banner__section-slider-btnprev"><img src="images/left-arrow.svg"" alt=""></button>',
+    nextArrow:
+      '<button class= "banner__section-slider-btn banner__section-slider-btnnext " > <img src="images/right-arrow.svg" alt=""></button>',
+  });
 
-    $('.tab').on('click', function (e) {
-        e.preventDefault();
+  $(".tab").on("click", function (e) {
+    e.preventDefault();
 
-        $($(this).siblings()).removeClass('tab-active');
-        $($(this).parent().siblings().find('div')).removeClass('tabs__content-active');
-        
-        $(this).addClass('tab-active');
-        $($(this).attr('href')).addClass('tabs__content-active');
-    });
+    $($(this).siblings()).removeClass("tab-active");
+    $($(this).parent().siblings().find("div")).removeClass(
+      "tabs__content-active"
+    );
 
-    $(".product__item-favourite").on('click', function () {
-        $(this).toggleClass('product__item-favourite--active');
-    })
+    $(this).addClass("tab-active");
+    $($(this).attr("href")).addClass("tabs__content-active");
+  });
 
-    $('.product__slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow: '<button class="product__slider-slider-btn product__slider-slider-btnprev"><img src="images/left-arrow-black.svg"" alt=""></button>',
-        nextArrow: '<button class= "product__slider-slider-btn product__slider-slider-btnnext " > <img src="images/right-arrow-black.svg" alt=""></button>'
+  $(".product__item-favourite").on("click", function () {
+    $(this).toggleClass("product__item-favourite--active");
+  });
 
-    });
+  $(".product__slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<button class="product__slider-slider-btn product__slider-slider-btnprev"><img src="images/left-arrow-black.svg"" alt=""></button>',
+    nextArrow:
+      '<button class= "product__slider-slider-btn product__slider-slider-btnnext"><img src="images/right-arrow-black.svg" alt=""></button>',
+  });
 
+  $(".filter-style").styler();
 
+  $(".filter__item-drop, .filter__extra").on("click", function () {
+    
 
+    $(this).toggleClass("filter__item-drop--active");
+    $(this).next().slideToggle(200);
+    e.preventDefault();
+  });
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 100000,
+    max: 500000
+  });
+
+  $(".catalog__filter-btngrid").on("click", function () {
+    $(this).addClass("catalog__filter-button--active");
+    $(".catalog__filter-btnline").removeClass("catalog__filter-button--active");
+    $(".product__item-wrapper").removeClass("product__item-wrapper--list");
+  });
+
+  $(".catalog__filter-btnline").on("click", function () {
+    $(this).addClass("catalog__filter-button--active");
+    $(".catalog__filter-btngrid").removeClass("catalog__filter-button--active");
+    $(".product__item-wrapper").addClass("product__item-wrapper--list");
+  });
+
+  $(".rateYo").rateYo({
+    rating: 3.6,
+    spacing: "7px",
+    ratedFill: "#1c62cd",
+    normalFill:"#c4c4c4"
+
+  });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,93 @@
-const { src, dest, watch, parallel, series } = require('gulp');
+const { src, dest, watch, parallel, series } = require("gulp");
 
-const scss = require('gulp-sass');
-const concat = require('gulp-concat')
-const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify-es').default;
-const autoprefixer = require('gulp-autoprefixer');
-const autoPrefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-const del = require('del');
+const scss = require("gulp-sass");
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create();
+const uglify = require("gulp-uglify-es").default;
+// const autoprefixer = require("gulp-autoprefixer");
+const autoPrefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
 
 function browsersync() {
-    browserSync.init({
-        server: {
-            baseDir: 'app/'
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: "app/",
+    },
+  });
 }
 
 function cleanDist() {
-    return del('dist')
+  return del("dist");
 }
 
 function images() {
-    return src(
-        'app/images/**/*')
-        .pipe(imagemin([
-            imagemin.gifsicle({ interlaced: true }),
-            imagemin.mozjpeg({ quality: 75, progressive: true }),
-            imagemin.optipng({ optimizationLevel: 5 }),
-            imagemin.svgo({
-                plugins: [
-                    { removeViewBox: true },
-                    { cleanupIDs: false }
-                ]
-            })
-        ]))
-        .pipe(dest('dist/images'))
+  return src("app/images/**/*")
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(dest("dist/images"));
 }
 
 function scripts() {
-    return src([
-
-        'node_modules/jquery/dist/jquery.js',
-        'app/js/main.js',
-        
-        'node_modules/slick-carousel/slick/slick.js'
-
-
-        
-    ])
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(dest('app/js'))
-        .pipe(browserSync.stream())
+  return src([
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/slick-carousel/slick/slick.js",
+    "node_modules/jquery-form-styler/dist/jquery.formstyler.js",
+        "node_modules/ion.rangeSlider-master/js/ion.rangeSlider.js",
+    "node_modules/rateyo/src/jquery.rateyo.js",
+    "node_modules/rateyo/src/jquery.rateyo.min.js",
+    "app/js/main.js",
+    // "app/js/main.min.js"
+  ])
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
 }
 
 function styles() {
-    return src(
-        'app/scss/style.scss'
+  return src([
+     "app/scss/style.scss",
+     "node_modules/ion.rangeSlider-master/css/ion.rangeSlider.css",
+    // "node_modules/rateyo/lib/iife/rateyo.css"
+   
+  ])
+    .pipe(scss({ outputStyle: "compressed" }))
+    .pipe(concat("style.min.css"))
+    .pipe(
+      autoPrefixer({
+        overrideBrowserslist: ["last 10 version"],
+        grid: true,
+      })
     )
-        .pipe(scss({ outputStyle: 'compressed' }))
-        .pipe(concat('style.min.css'))
-        .pipe(autoPrefixer({
-            overrideBrowserslist: ['last 10 version'],
-            grid: true
-        }))
-        .pipe(dest('app/css'))
-        .pipe(browserSync.stream());
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
 }
 
 function build() {
-    return src(['app/css/style.min.css',
-        'app/fonts/**/*',
-        'app/js/main.min.js',
-        'app/*.html'
-    ], { base: 'app' })
-        .pipe(dest('dist'))
+  return src(
+    [
+      "app/css/style.min.css",
+      "app/fonts/**/*",
+      "app/js/main.min.js",
+      "app/*.html",
+    ],
+    { base: "app" }
+  ).pipe(dest("dist"));
 }
 
 function watching() {
-    watch(['app/scss/**/*.scss'], styles);
-    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-    watch(['app/*.html']).on('change', browserSync.reload);
+  watch(["app/scss/**/*.scss"], styles);
+  watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
+  watch(["app/*.html"]).on("change", browserSync.reload);
 }
 
 exports.styles = styles;
@@ -93,4 +99,3 @@ exports.images = images;
 exports.build = series(cleanDist, images, build);
 
 exports.default = parallel(styles, scripts, browsersync, watching);
-
